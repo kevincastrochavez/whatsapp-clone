3f993a96753efcf9204f263f7fc57368fc49b8f7
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-13 19:10:18 -0600

    created the sidebar component and inside, header, search and chats components. Installed material-ui and integrated some icons. Made api call to generate random avatars for each chat. Styled a little

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div className="app">Whatsapp Clone</div>;
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+
+        <div className="sidebar__header-right">
+          <IconButton>
+            <DonutLarge />
+          </IconButton>
+
+          <IconButton>
+            <Chat />
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__search-container">
+          <SearchOutlined />
+
+          <input type="text" placeholder="Search or start a new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
